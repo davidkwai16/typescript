cf5d2a76b06a1b20220e60fefba680b824ae0de6
davidkwai16 <dlkp16@gmail.com>
2025-02-24 17:04:18 -0300

    Yup

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,17 +1,34 @@
 import { Request, Response } from "express";
+//import { StatusCodes } from "http-status-codes";
+import * as yup from 'yup';
 
 interface ICidade{
     nome: string;
 }
 
-
+const bodyValidation: yup.Schema<ICidade>  = yup.object().shape({
+    nome: yup.string().required().min(3),
+});
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export const create = (req: Request<{},{},ICidade>, res: Response) => {
+export const create = async (req: Request<{},{},ICidade>, res: Response) => {
 
-    const data: ICidade = req.body;
+    let validatedData: ICidade | undefined = undefined;
 
-    console.log(data.nome);
+    try {
+        validatedData = await bodyValidation.validate(req.body);
+    } catch (error) {
+        const yupError = error as yup.ValidationError;
+
+        return res.json({
+            errors: {
+                default: yupError.message,
+            }
+        })
+    }
+
+
+    console.log(validatedData);
 
     return res.send('Create!');
 }
