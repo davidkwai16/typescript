8dd52dd383012d8c6ec1cbdd15236de5453fe8bd
davidkwai16 <dlkp16@gmail.com>
2025-03-26 16:38:28 -0300

    provider

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -3,6 +3,7 @@ import * as yup from 'yup';
 import { validation } from "../../shared/middleware";
 import { StatusCodes } from "http-status-codes";
 import { ICidade } from "../../database/models";
+import { CidadesProvider } from "../../database/providers/cidades";
  
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
@@ -10,15 +11,21 @@ interface IBodyProps extends Omit<ICidade, 'id'> { }
 
 export const createValidation = validation((getSchema) => ({
     body: getSchema<IBodyProps>(yup.object().shape({
-        nome: yup.string().required().min(3),
+        nome: yup.string().required().min(3).max(150),
     })),
 }));
 
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export const create = async (req: Request<{},{},ICidade>, res: Response) => {
+    const result = await CidadesProvider.create(req.body);
+    if (result instanceof Error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            error: {
+                default: result.message
+            }
+        });
+    }
 
-    console.log(req.body);
-
-    return res.status(StatusCodes.CREATED).json(1);
+    return res.status(StatusCodes.CREATED).json(result);
 };
