08da0c76127463a64d163bc866fe944a71979312
davidkwai16 <dlkp16@gmail.com>
2025-05-16 10:39:00 -0300

    autenticação

---
+++ src/server/shared/middleware/EnsureAuthenticated.ts
@@ -0,0 +1,30 @@
+import { RequestHandler } from "express";
+import { StatusCodes } from "http-status-codes";
+
+export const ensureAuthenticated: RequestHandler = async (req, res, next) => {
+    const { authorization } = req.headers;    
+
+    if (!authorization) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        error: { default: 'Não autenticado' }
+      });  
+    }
+
+    console.log(authorization);
+
+    const [type, token] = authorization.split(' ');
+
+    if (type !== 'Bearer') {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        error: { default: 'Não autenticado' }
+      });  
+    }
+
+    if (token !== 'teste.teste.teste') {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        error: { default: 'Não autenticado' }
+      });  
+    }
+
+    return next();
+}
